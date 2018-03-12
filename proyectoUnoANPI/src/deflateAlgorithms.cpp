a54264e29e77ad1dd547c84c27e97acdc8415255
arielm7 <arielmv07@gmail.com>
2018-03-12 12:16:03 -0600

    stop gitignore

---
+++ proyectoUnoANPI/src/deflateAlgorithms.cpp
@@ -0,0 +1,21 @@
+/*
+ * deflateAlgorithms.cpp
+ *
+ *  Created on: mar. 2018
+ *      Author: ariel
+ */
+
+#include <boost/math/tools/polynomial.hpp>
+#include <complex>
+using namespace boost::math;
+using namespace boost::math::tools; // for polynomial
+
+template<typename T>
+polynomial<T> deflate(const polynomial<T>& poly, const T& root,
+polynomial<T>& residuo){
+
+	//code in here please
+
+}
+
+
---
+++ proyectoUnoANPI/src/proyectoUnoANPI.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : proyectoUnoANPI.cpp
+// Author      : Ariel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Main
+//============================================================================
+
+
+#include <iostream>
+#include <boost/math/tools/polynomial.hpp>
+#include <iomanip>
+#include <complex>
+#include <cmath>
+#include "deflateAlgorithms.cpp"
+using namespace std;
+//using namespace std::literals::complex_literals;
+using namespace boost::math;
+using namespace boost::math::tools; // for polynomial
+int main() {
+	complex<double> x=7;
+	complex<double> y=9;
+	//polynomial<complex<double>> const b{{1,2}};
+	cout << "hola a todos" << endl; // prints hola
+	return 0;
+}
