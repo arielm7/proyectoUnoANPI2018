1d9691b5a2ec5ee3b19daec2e94e2ef3193d5395
luisf1997cr <luismuelino@gmail.com>
2018-03-12 12:20:45 -0600

    Prueba

---
+++ proyectoUnoANPI/src/Metodos/laguerre/laguerre.h
@@ -0,0 +1,30 @@
+/*
+ * laguerre.h
+ *
+ *  Created on: 12 mar. 2018
+ *      Author: acacia
+ */
+
+#ifndef METODOS_LAGUERRE_LAGUERRE_H_
+#define METODOS_LAGUERRE_LAGUERRE_H_
+
+template<class T>
+class laguerre {
+public:
+	laguerre();
+	virtual ~laguerre();
+};
+
+template<class T>
+laguerre<T>::laguerre() {
+	// TODO Auto-generated constructor stub
+
+}
+
+template<class T>
+laguerre<T>::~laguerre() {
+	// TODO Auto-generated destructor stub
+}
+
+
+#endif /* METODOS_LAGUERRE_LAGUERRE_H_ */
