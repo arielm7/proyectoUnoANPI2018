08712850af884887eb46c1e3e7ae59803d6aa187
arielm7 <arielmv07@gmail.com>
2018-03-14 17:21:45 -0600

    deflate funciona para reales y deflate2 aun no está del todo bien

--- proyectoUnoANPI/src/deflateAlgorithms.cpp
+++ proyectoUnoANPI/src/deflateAlgorithms.cpp
@@ -7,15 +7,55 @@
 
 #include <boost/math/tools/polynomial.hpp>
 #include <complex>
+#include <iostream>
+
 using namespace boost::math;
 using namespace boost::math::tools; // for polynomial
 
 template<typename T>
-polynomial<T> deflate(const polynomial<T>& poly, const T& root,
-polynomial<T>& residuo){
+polynomial<T> deflate(const polynomial<T>& poly, const T& root, T& residuo){
 
-	//code in here please
+	polynomial<T>  result=poly;
+	int n=poly.size()-1;
+	residuo=poly[n];
+	result[n]=T(0);
+	for(int i=n-1;i>=0;i--) {
 
-}
+		T swap=result[i];
+		result[i]=residuo;
+		residuo=swap+residuo*root;
 
+
+		}
+	return result;
+}//end deflate
+
+template<typename T>
+polynomial<T> deflate2(const polynomial<T>& poly, const
+		std::complex<T>& root,polynomial<T>& residuo){
+
+	polynomial<T> result=poly;
+	polynomial<T>	cuadratic={{(root.real()*root.real())+(root.imag()*root.imag()),-2*root.real(),1}};
+	std::cout<<root.imag()<<"cuadra"<<std::endl;
+	int n=poly.size()-1;
+	residuo=poly;
+
+	int k,j;
+	for (j=0;j<=n;j++) {
+
+	result[j]=T(0);
+	}
+
+	for (k=n-2;k>=0;k--) {
+	result[k]=residuo[k+2];
+	//std::cout<<result[k]<<std::endl;
+	for (j=k+1;j>=k;j--){ residuo[j] -= result[k]*cuadratic[j-k];
+	std::cout<<cuadratic[j-k]<<std::endl;}
+	}
+	for (j=2;j<=n;j++) residuo[j]=T(0);
+
+	return result;
+
+
+}//end deflate2
 
--- proyectoUnoANPI/src/proyectoUnoANPI.cpp
+++ proyectoUnoANPI/src/proyectoUnoANPI.cpp
@@ -14,13 +14,18 @@
 #include <cmath>
 #include "deflateAlgorithms.cpp"
 using namespace std;
-//using namespace std::literals::complex_literals;
+
 using namespace boost::math;
 using namespace boost::math::tools; // for polynomial
 int main() {
 	complex<double> x=7;
 	complex<double> y=9;
-	//polynomial<complex<double>> const b{{1,2}};
-	cout << "hola a todos" << endl; // prints hola
+	polynomial<double> const c{{4,0,1}};
+	polynomial<double> resi ={{0,0,0}};
+	boost::array<double, 3> a = {{-4, 0,1}};
+
+	complex<double> const j(1,-3);
+	cout << deflate2(c,j,resi) << endl; // prints hola
+	cout<<"residuo: "<<resi<<endl;
 	return 0;
 }
